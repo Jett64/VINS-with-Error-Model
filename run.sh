39775dd1c54ba65016c1a97f1e611c58a10999a5
Jett64 <m15518118471@gmail.com>
2025-01-15 19:26:32 +0800

    First commit

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+gnome-terminal --tab -- bash -c "cd ~/catkin_ws/;
+				source ./devel/setup.bash;
+				roscore;
+				exec bash"
+
+sleep 2
+
+gnome-terminal --tab -- bash -c "cd ~/catkin_ws/;
+				source ./devel/setup.bash;
+				roslaunch estimator euroc.launch;
+				exec bash"
+
+gnome-terminal --tab -- bash -c "cd ~/catkin_ws/;
+				source ./devel/setup.bash;
+				roslaunch estimator rviz.launch;
+				exec bash"
+
+# gnome-terminal --tab -- bash -c "cd ~/catkin_ws/;
+# 				source ./devel/setup.bash;
+# 				roslaunch benchmark_publisher publish.launch  sequence_name:=V2_03_difficult;
+# 				exec bash"
+
+gnome-terminal --tab -- bash -c "cd ~/catkin_ws/;
+				source ./devel/setup.bash;
+				rosbag play /home/Jett64/catkin_ws/data/EuRoC/MH_01_easy.bag -s 0 -r 1;
+				exec bash"
